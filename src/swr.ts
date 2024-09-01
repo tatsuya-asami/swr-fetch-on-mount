7704ceb4231b489b2f71bdfc77c0bd7f3d9d9f58
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-09-01 12:54:37 +0900

    feat: Refactor useFetchSWR to fetch user and post data

--- src/swr.ts
+++ src/swr.ts
@@ -1,12 +1,35 @@
-import useSWR, { SWRConfiguration } from "swr";
+import useSWR, { SWRConfiguration, Key, SWRResponse } from "swr";
 import useSWRMutation from "swr/mutation";
+import { ENDPOINTS, Post, User } from "./api/handlers";
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-export const useFetchSWR = (key: string, config?: SWRConfiguration) => {
+const useFetchSWR = <Data, Error = any, SWRKey extends Key = Key>(
+  key: SWRKey,
+  config?: SWRConfiguration
+): SWRResponse<Data, Error> => {
   return useSWR(key, fetcher, config);
 };
 
-export const useFetchSWRMutation = (key: string) => {
-  return useSWRMutation(key, fetcher);
+export const useFetchUsers = () => {
+  return useFetchSWR<User[]>(ENDPOINTS.USER);
+};
+export const useFetchPosts = (
+  users: User[] | undefined,
+  userId: number | undefined
+) => {
+  const query = userId ? `?userId=${userId}` : "";
+  return useFetchSWR<User[]>(users ? `${ENDPOINTS.POSTS}${query}` : null);
+};
+
+export const useFetchPostsByMutation = () => {
+  type Query = { userId: number };
+  const getPosts = async (path: string, { arg }: { arg: Query }) => {
+    const query = arg.userId
+      ? new URLSearchParams({ userId: arg.userId.toString() })
+      : undefined;
+    return fetch(arg.userId ? `${path}?${query}` : path).then((r) => r.json());
+  };
+
+  return useSWRMutation<Post[], any, string, Query>(ENDPOINTS.POSTS, getPosts);
 };
