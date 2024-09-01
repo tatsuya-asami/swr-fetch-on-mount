cb1ac5da3d0ad783e7a140b20f4aa48cd5b5d897
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-09-01 11:58:15 +0900

    feat: Update API handlers to fetch user and post data

--- src/api/handlers.ts
+++ src/api/handlers.ts
@@ -2,28 +2,66 @@ import { http, HttpResponse, delay } from "msw";
 
 export const ENDPOINTS = {
   USER: "https://example.com/user",
-  POST: "https://example.com/post",
-} as const
+  POSTS: "https://example.com/users",
+} as const;
 
 export const handlers = [
   http.get(ENDPOINTS.USER, async () => {
     await delay(1000);
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      postId: 1,
-      firstName: "John",
-      lastName: "Maverick",
-    });
+    return HttpResponse.json(USERS);
   }),
-  http.get(ENDPOINTS.POST, async () => {
+  http.get(ENDPOINTS.POSTS, async ({ request }) => {
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("userId");
     await delay(1000);
-
-    return HttpResponse.json({
-      user: {
-        id: 1,
-        firstName: "John",
-        lastName: "Maverick",
-      },
-    });
+    if (!userId) {
+      return HttpResponse.json(POSTS);
+    }
+    return HttpResponse.json(
+      POSTS.filter((post) => post.userId === parseInt(userId))
+    );
   }),
 ];
+
+export type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
+};
+
+export const USERS = [
+  {
+    id: 1,
+    firstName: "Susumu",
+    lastName: "Kurose",
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Maverick",
+  },
+] as const satisfies User[];
+
+export type Post = {
+  id: number;
+  userId: number;
+  body: string;
+};
+const POSTS = [
+  {
+    id: 1,
+    userId: 2,
+    body: "userId 2 John",
+  },
+  {
+    id: 2,
+    userId: 1,
+    body: "userId 1 Susumu",
+  },
+  {
+    id: 3,
+    userId: 2,
+    body: "userId 2 John2",
+  },
+  { id: 4, userId: 3, body: "userId 3 unknown" },
+] as const satisfies Post[];
